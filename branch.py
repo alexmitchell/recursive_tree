1173115809d67f99df2db2b06ce17861f92144f8
Alex Mitchell <ebpvyeuj@gmail.com>
2015-03-09 13:32:31 -0700

    Added user input methods and random procreation.
    Can adjust turn angle, split angle, attenuation, and branch vector
    length for the root vector, but the adjustments will propagate to
    downstream branches.
    
    Can generate a new tree with the current parameter values.
    
    Branches will procreate randomly as a function of depth. Branches deeper
    on the tree are less likely to have child branches.

--- branch.py
+++ branch.py
@@ -1,14 +1,27 @@
+from random import random
 
 class Branch:
     max_depth = 1
-    def __init__(self, depth, base_vector, branch_vector, attenuation,
+    def __init__(self, depth):
+        # Current depth of tree starting at 0.
+        # Will be less than Branch.max_depth
+        self.depth = depth
+
+        # Procreate
+        child_depth = depth + 1
+        self.handed_child = None
+        self.second_child = None
+        if (child_depth < Branch.max_depth):
+            if random() < 0.98**child_depth:
+                self.handed_child = Branch(child_depth)
+            if random() < 0.98**child_depth:
+                self.second_child = Branch(child_depth)
+
+    def set_branch(self, base_vector, branch_vector, attenuation,
                  handed, turn, split):
         # The variables are present in the order that they will be 
         # calculated.
 
-        # Current depth of tree starting at 0.
-        # Will be less than Branch.max_depth
-        self.depth = depth
         # Position vector of the base of the branch.
         self.base_vector = base_vector
         # Vector representing the branch.
@@ -23,37 +36,35 @@ class Branch:
         # Angle between the children branches.
         self.split = split
 
-        self.handed_child = None
-        self.second_child = None
-
-    def generate_tree_recursion(self):
+    def update_tree_recursion(self):
 
         child_depth = self.depth + 1
         if (child_depth < Branch.max_depth):
             base_vector = self.base_vector + self.branch_vector
 
             # Calculate the child vectors.
-            proto_vector = self.branch_vector * self.attenuation
-            handed_vector = proto_vector.get_rotated(self.handed * self.turn)
-            second_vector = handed_vector.get_rotated(-self.handed * self.split)
+            attenuated_vector = self.branch_vector * self.attenuation
+            handed_vector = attenuated_vector.get_rotated(
+                    self.handed * self.turn)
+            second_vector = handed_vector.get_rotated(
+                    -self.handed * self.split)
 
-            # Modify these variables as a function of depth for cooler 
-            # effects.
+            # Children geometry variables
             attenuation = self.attenuation
             turn = self.turn
             split = self.split
 
-            # Make the children.
-            self.handed_child = Branch(
-                    child_depth, base_vector, handed_vector,
-                    attenuation, self.handed, turn, split)
-            self.second_child = Branch(
-                    child_depth, base_vector, second_vector,
-                    attenuation, -self.handed, turn, split)
-
-            # Recurse.
-            self.handed_child.generate_tree_recursion()
-            self.second_child.generate_tree_recursion()
+            # Set or reset the children then recurse.
+            if self.handed_child is not None:
+                self.handed_child.set_branch(
+                        base_vector, handed_vector,
+                        attenuation, self.handed, turn, split)
+                self.handed_child.update_tree_recursion()
+            if self.second_child is not None:
+                self.second_child.set_branch(
+                        base_vector, second_vector,
+                        attenuation, -self.handed, turn, split)
+                self.second_child.update_tree_recursion()
 
     def draw_recursive(self, draw_line):
         x0, y0 = self.base_vector.get_tuple()
@@ -66,3 +77,22 @@ class Branch:
         if self.second_child is not None:
             self.second_child.draw_recursive(draw_line)
 
+    def change_turn(self, turn):
+        # Update the turn variable for all downstream branches
+        self.turn = turn
+        self.update_tree_recursion()
+
+    def change_split(self, split):
+        # Update the split variable for all downstream branches
+        self.split = split
+        self.update_tree_recursion()
+
+    def change_attenuation(self, attenuation):
+        # Update the attenuation variable for all downstream branches
+        self.attenuation = attenuation
+        self.update_tree_recursion()
+
+    def change_branch_vector(self, branch_vector):
+        # Update the attenuation variable for all downstream branches
+        self.branch_vector = branch_vector
+        self.update_tree_recursion()
--- root.py
+++ root.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python3
 
 import pyglet
+from pyglet.window import key
 
 from vecrec import Vector
 from math import pi
 from branch import Branch
         
-# Create the tree
-max_depth = 10
-depth = 0
-base_vector = Vector(350, 0)
-branch_vector = Vector(0, 200)
-attenuation = 0.6
-handed = 1
-turn = 3*pi/12
-split = 4*pi/12
-
-Branch.max_depth = max_depth
-root = Branch(
-        depth, base_vector, branch_vector,
-        attenuation, handed, turn, split)
-
-root.generate_tree_recursion()
-
-# Display the tree
 window = pyglet.window.Window()
 
+# Default variable values
+max_depth = 10
+depth = 0
+base_vector = Vector(window.width/2, 0)
+branch_vector = Vector(0, window.height/3)
+init_attenuation = 0.6
+init_handed = 1
+init_turn = 45 #3*pi/12
+init_split = 60 #4*pi/12
+
+def generate_tree():
+    global max_depth, base_vector, branch_vector, init_attenuation
+    global init_handed, init_turn, init_split
+
+    Branch.max_depth = max_depth
+    root = Branch(depth)
+    root.set_branch(
+            base_vector, branch_vector, init_attenuation,
+            init_handed, init_turn*pi/180, init_split*pi/180)
+    root.update_tree_recursion()
+
+    return root
+
+
+root = generate_tree()
+
+# Display the tree
 def draw_line(coordinates):
     pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2f', coordinates))
 
@@ -34,5 +44,62 @@ def on_draw():
     window.clear()
     root.draw_recursive(draw_line)
 
-pyglet.app.run()
 
+# User input
+turn_step = 5 #pi / 30
+split_step = 5 #pi / 30
+attenuation_step = 0.01
+branch_vector_step = branch_vector/10
+
+@window.event
+def on_key_press(symbol, modifier):
+    global init_turn, init_split, init_attenuation, branch_vector
+
+    if symbol == key.UP:
+        init_turn += turn_step
+        root.change_turn(init_turn*pi/180)
+        print("Changing turn angle to", init_turn)#*180/pi)
+
+    elif symbol == key.DOWN:
+        init_turn -= turn_step
+        root.change_turn(init_turn*pi/180)
+        print("Changing turn angle to", init_turn)#*180/pi)
+
+    elif symbol == key.RIGHT:
+        init_split += split_step
+        root.change_split(init_split*pi/180)
+        print("Changing split angle to", init_split)#*180/pi)
+
+    elif symbol == key.LEFT:
+        init_split -= split_step
+        root.change_split(init_split*pi/180)
+        print("Changing split angle to", init_split)#*180/pi)
+
+    elif symbol == key.A:
+        init_attenuation += attenuation_step
+        root.change_attenuation(init_attenuation)
+        print("Changing attenuation to", init_attenuation)
+
+    elif symbol == key.S:
+        init_attenuation -= attenuation_step
+        root.change_attenuation(init_attenuation)
+        print("Changing attenuation to", init_attenuation)
+
+    elif symbol == key.Q:
+        branch_vector += branch_vector_step
+        root.change_branch_vector(branch_vector)
+        print("Changing root branch vector to", branch_vector)
+
+    elif symbol == key.W:
+        branch_vector -= branch_vector_step
+        root.change_branch_vector(branch_vector)
+        print("Changing root branch vector to", branch_vector)
+
+    elif symbol == key.BACKSPACE:
+        global root
+        # root.teardown()
+        root = generate_tree()
+        print("Generating a new tree")
+
+
+pyglet.app.run()
